aacd163304a059a3db9001c0c1b56688bf4a63db
Danyael Natan Vasquez Ojeda <dnvasquez@iplacex.cl>
2026-01-25 09:36:31 -0300

    ADD CORS nuestra boda 2026

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://nuestraboda2026.cl',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
